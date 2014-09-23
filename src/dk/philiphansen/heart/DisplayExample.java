b0f33a3c2c931f48c8eea3f8f2a59ad53fc2e39b
Philip Hansen <me@philiphansen.dk>
2014-09-23 03:06:46 +0100

    Basic drawing

--- src/dk/philiphansen/heart/DisplayExample.java
+++ src/dk/philiphansen/heart/DisplayExample.java
@@ -1,8 +1,7 @@
 package dk.philiphansen.heart;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.*;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
@@ -18,11 +17,37 @@ public class DisplayExample {
 			System.exit(0);
 		}
 
-		// init OpenGL here
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
 		while (!Display.isCloseRequested()) {
 
-			// render OpenGL here
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			GL11.glColor3f(0.5f,0.5f,1.0f);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(100,100);
+			GL11.glVertex2f(100+100,100);
+			GL11.glVertex2f(100+100,100+100);
+			GL11.glVertex2f(100,100+100);
+			GL11.glEnd();
+
+			GL11.glColor3f(0.5f,1.0f, 0.5f);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(300,100);
+			GL11.glVertex2f(300+300,100);
+			GL11.glVertex2f(300+300,100+300);
+			GL11.glVertex2f(300,100+300);
+			GL11.glEnd();
+
+			GL11.glColor3f(1.0f,0.5f, 0.5f);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(150,150);
+			GL11.glVertex2f(150+200,150);
+			GL11.glVertex2f(150+200,150+200);
+			GL11.glVertex2f(150,150+200);
+			GL11.glEnd();
 
 			Display.update();
 		}
